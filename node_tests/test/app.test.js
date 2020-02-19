77e91235999fd25a8edd6e467ff52070d0d134fd
Heidrun Heidarsdottir <heidrunh17@ru.is>
2020-02-19 00:15:21 +0000

    Set up files for lab class 5

--- node_tests/test/app.test.js
+++ node_tests/test/app.test.js
@@ -1,10 +1,10 @@
 var expect = require('chai').expect;
 const d4 = require("./../app.js");
 
-describe('d4()', function() {
+describe('p1 test', function() {
     var arr = ["one", 2, "three"];
     var revArr = ["three", 2, "one"];
-    it("p1 test: Function should return the reversed array", function() {
+    it("Function should return the reversed array", function() {
         expect(d4.p1(arr)).to.deep.equal(revArr);
     });
 });
